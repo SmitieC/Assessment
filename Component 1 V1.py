51477fc91db07098272c80046295e2a28f1c973a
SmitieC <83625219+SmitieC@users.noreply.github.com>
2023-05-04 09:08:55 +1200

    main

---
+++ Component 1 V1.py
@@ -0,0 +1,11 @@
+"""Car Assessment Game -- Compenent 1 Version 1
+Version 1 to Initilize Screen Size
+By Conor Smith"""
+
+import pygame
+import time
+
+pygame.init()
+screen = pygame.display.set_mode((700, 925))
+
+time.sleep(5)
