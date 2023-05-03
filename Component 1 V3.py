fbf923b3710d0dd576be547f6363434d3041d6cb
SmitieC <83625219+SmitieC@users.noreply.github.com>
2023-05-04 09:21:46 +1200

    Component 1

---
+++ Component 1 V3.py
@@ -0,0 +1,23 @@
+"""Car Assessment Game -- Compenent 1 Version 3
+Version 3 to Set Game Icon
+By Conor Smith"""
+
+# Imports the Pygame library for graphics and the Time library for pausing
+import pygame
+import time
+
+pygame.init()
+
+# Creates a Pygame window with a size of 700 pixels wide and 925 pixels tall
+screen = pygame.display.set_mode((700, 925))
+
+# Set Window Name to be Car Driving Game
+pygame.display.set_caption("Car Driving Game")
+
+# Set Window Icon
+game_icon = pygame.image.load('game_icon.png')
+pygame.display.set_icon(game_icon)
+
+
+# Pauses the program for 5 seconds before it closes
+time.sleep(5)
