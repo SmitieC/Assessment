53244e5f1a7d1dfc634bc60b4dc335bb76b463f5
SmitieC <83625219+SmitieC@users.noreply.github.com>
2023-05-04 09:35:29 +1200

    Component 2

---
+++ Component 2 V1.py
@@ -0,0 +1,34 @@
+"""Car Assessment Game -- Compenent 2 Version 1
+Version 1 to Create Running Game loop
+By Conor Smith"""
+
+# Imports the Pygame library for graphics and the Time library for pausing
+import pygame
+import time
+
+pygame.init()
+
+# Component 1 To Initlize Screen
+screen = pygame.display.set_mode((700, 925))
+pygame.display.set_caption("Car Driving Game")
+game_icon = pygame.image.load('game_icon.png')
+pygame.display.set_icon(game_icon)
+
+# initialize clock to control frame rate
+clock = pygame.time.Clock()
+
+# Game Variables
+game_over = False
+
+
+# Game Loop
+while True:
+    if not game_over:
+    
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                game_over = True
+                pygame.quit()
+                quit()
+
+    clock.tick(60)
