b65d91d087fc034a4388d021994f46ca96cdeef4
SmitieC <83625219+SmitieC@users.noreply.github.com>
2023-05-04 11:35:25 +1200

    component 2

---
+++ Component 2 V3.py
@@ -0,0 +1,55 @@
+"""Car Assessment Game -- Compenent 2 Version 3
+Version 3 to Create controls for driver class
+By Conor Smith"""
+
+# Imports the Pygame library for graphics and the Time library for pausing
+import pygame
+import time
+
+from pygame.sprite import AbstractGroup
+
+pygame.init()
+
+# Component 1 To Initlize Screen
+screen = pygame.display.set_mode((700, 925))
+pygame.display.set_caption("Car Driving Game")
+game_icon = pygame.image.load('game_icon.png')
+pygame.display.set_icon(game_icon)
+
+# initialize clock to control frame rate
+clock = pygame.time.Clock()
+
+# class for player driven car
+class Driver(pygame.sprite.Sprite):
+    def __init__(self, *groups: AbstractGroup) -> None:
+        super().__init__(*groups)
+        self.image = pygame.image.load('driver_car.png')
+        self.x_pos = x_pos
+        self.y_pos = y_pos
+        self.rect = self.image.get_rect(center=(self.x_pos, self.y_pos))
+    
+    def move_right:
+        self.image.transform.rotate("", 30)
+        self.x_pos += VELOCITY
+    
+    def move_left:
+        self.image = pygame.image.load()  # make car look like turning
+        self.x_pos -= VELOCITY
+
+# Game Variables and constants
+game_over = False
+VELOCITY = 5
+
+
+# Game Loop
+while True:
+    if not game_over:
+    
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                game_over = True
+                pygame.quit()
+                quit()
+
+    clock.tick(60)
+    pygame.display.update()
