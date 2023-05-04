6f8d5320d3985c758c047bb9fdebb2cf23633ef2
SmitieC <83625219+SmitieC@users.noreply.github.com>
2023-05-05 11:45:40 +1200

    component 2

--- Component 2 V3.py
+++ Component 2 V3.py
@@ -1,5 +1,5 @@
 """Car Assessment Game -- Compenent 2 Version 3
-Version 3 to Create controls for driver class
+Version 3 to Print Driver car on screen
 By Conor Smith"""
 
 # Imports the Pygame library for graphics and the Time library for pausing
@@ -21,24 +21,34 @@ clock = pygame.time.Clock()
 
 # class for player driven car
 class Driver(pygame.sprite.Sprite):
-    def __init__(self, *groups: AbstractGroup) -> None:
+    def __init__(self, x_pos, y_pos, *groups: AbstractGroup) -> None:
         super().__init__(*groups)
-        self.image = pygame.image.load('driver_car.png')
+        self.image = pygame.transform.scale(pygame.image.load('driver_car.png'), (73, 125))
         self.x_pos = x_pos
         self.y_pos = y_pos
         self.rect = self.image.get_rect(center=(self.x_pos, self.y_pos))
     
-    def move_right:
+    def move_right(self):
         self.image.transform.rotate("", 30)
         self.x_pos += VELOCITY
     
-    def move_left:
-        self.image = pygame.image.load()  # make car look like turning
+    def move_left(self):
+        self.image.transform.rotate("", -30)  # make car look like turning
         self.x_pos -= VELOCITY
+    
+    def move_up(self):
+        self.image.transform.rotate("", 0)  # make car look like stright
+        
 
 # Game Variables and constants
 game_over = False
 VELOCITY = 5
+
+driver_group = pygame.sprite.GroupSingle()
+
+driver = Driver(350, 700)
+driver_group.add(driver)
+
 
 
 # Game Loop
@@ -50,6 +60,14 @@ while True:
                 game_over = True
                 pygame.quit()
                 quit()
+            
+            driver = Driver(350, 700)
+            driver_group.add(driver)
+
+    if not game_over:
+        driver_group.update()
+        driver_group.draw(screen)
+    
 
     clock.tick(60)
     pygame.display.update()
