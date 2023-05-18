b3db0160fb2136e1b40f06c5de79bf7466c70cb3
SmitieC <83625219+SmitieC@users.noreply.github.com>
2023-05-19 11:45:36 +1200

    component 3

---
+++ Component 3 V1 trial 2.py
@@ -0,0 +1,100 @@
+"""Car Assessment Game -- Component 3 Version 1
+Version 1 to make backround loop seemlessly
+By Conor Smith"""
+
+# Imports the Pygame library for graphics and the Time library for pausing
+import pygame
+import time
+import math
+
+from pygame.sprite import AbstractGroup
+
+pygame.init()
+
+# Component 1 To Initialize Screen
+screen = pygame.display.set_mode((700, 925))
+pygame.display.set_caption("Car Driving Game")
+game_icon = pygame.image.load('game_icon.png')
+pygame.display.set_icon(game_icon)
+
+# Initialize clock to control frame rate
+clock = pygame.time.Clock()
+
+# Class for player driven car
+class Driver(pygame.sprite.Sprite):
+    def __init__(self, x_pos, y_pos, *groups: AbstractGroup) -> None:
+        super().__init__(*groups)
+        self.orig_image = pygame.transform.scale(pygame.image.load('driver_car.png'), (73, 125))
+        self.image = self.orig_image
+        self.x_pos = x_pos
+        self.y_pos = y_pos
+        self.rect = self.image.get_rect(center=(self.x_pos, self.y_pos))
+        self.angle = 0
+
+    def move_right(self):
+        self.angle = -15
+        if self.x_pos < 700 - VELOCITY:  # check if car is within screen
+            self.x_pos += VELOCITY
+
+    def move_left(self):
+        self.angle = 15  # make car look like turning
+        if self.x_pos > VELOCITY:  # check if car is within screen
+            self.x_pos -= VELOCITY
+
+    def move_up(self):
+        self.angle = 0  # make car look like straight
+
+    def update(self):
+        self.image = pygame.transform.rotate(self.orig_image, self.angle)
+        self.rect = self.image.get_rect(center=(self.x_pos, self.y_pos))
+        self.rect = self.rect.clamp(screen.get_rect())
+
+# Game Variables and constants
+game_over = False
+VELOCITY = 5
+
+road = pygame.image.load('Road-1.png')
+road = pygame.transform.rotate(road, 90)
+road = pygame.transform.scale(road, (700, 925))
+
+driver_group = pygame.sprite.GroupSingle()
+
+driver = Driver(350, 700)
+driver_group.add(driver)
+
+# Initialize y-position of road
+road_y = -925
+
+# Game Loop
+while True:
+    if not game_over:
+
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_RIGHT] and not keys[pygame.K_LEFT]:
+            driver.move_right()
+        elif keys[pygame.K_LEFT] and not keys[pygame.K_RIGHT]:
+            driver.move_left()
+        else:
+            driver.move_up()
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                game_over = True
+
+        # Decrease y-position of road to make it move downwards
+        road_y += VELOCITY
+        if road_y >= 0:
+            road_y = -925
+
+    if not game_over:
+        driver_group.update()
+        screen.fill((255, 255, 255))  # Fill the screen with white before drawing the sprite
+
+        # Draw the road at its current y-position
+        screen.blit(road, (0, road_y))
+        screen.blit(road, (0, road_y + 925))
+
+        driver_group.draw(screen)
+
+    clock.tick(60)
+    pygame.display.update()
