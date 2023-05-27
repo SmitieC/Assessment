f06ac8b3e354f6fa89bb1df442c93bd2db31b3d9
SmitieC <83625219+SmitieC@users.noreply.github.com>
2023-05-27 14:04:52 +1200

    pep8 checks

--- Fully working game.py
+++ Fully working game.py
@@ -2,23 +2,18 @@
 Fully working Game
 By Conor Smith"""
 
-# Imports the Pygame library for graphics and the Time library for pausing
 import pygame
-import time
-import math
 import random
-
 from pygame.sprite import AbstractGroup
 
 pygame.init()
+
 
 # game loop function so game can be restarted
 def game_loop():
     # Component 1 To Initialize Screen
     global screen
     screen = pygame.display.set_mode((700, 925))
-    font = pygame.font.Font(None, 36)
-    
     pygame.display.set_caption("Car Driving Game")
     game_icon = pygame.image.load('game_icon.png')
     pygame.display.set_icon(game_icon)
@@ -26,16 +21,17 @@ def game_loop():
     # Initialize clock to control frame rate
     clock = pygame.time.Clock()
 
-    # initialize score
+    # Initialize score and font
     global score
     score = 0
-
+    font = pygame.font.Font(None, 36)
 
     # Class for player driven car
     class Driver(pygame.sprite.Sprite):
         def __init__(self, x_pos, y_pos, *groups: AbstractGroup) -> None:
             super().__init__(*groups)
-            self.orig_image = pygame.transform.scale(pygame.image.load('driver_car.png'), (73, 125))
+            self.orig_image = pygame.transform.scale(
+                pygame.image.load('driver_car.png'), (73, 125))
             self.image = self.orig_image
             self.x_pos = x_pos
             self.y_pos = y_pos
@@ -76,32 +72,25 @@ def game_loop():
                 pygame.image.load("car_5.png"), (73, 125)))
             self.running_sprites.append(pygame.transform.scale(
                 pygame.image.load("car_6.png"), (73, 125)))
-            
+
             self.image = random.choice(self.running_sprites)
             if self.x_pos > 400:
                 self.image = pygame.transform.rotate(self.image, 180)
             self.rect = self.image.get_rect(center=(self.x_pos, self.y_pos))
             self.speed = random.randint(10, 15)
 
-        
         def update(self):
             self.y_pos += self.speed  # Move obstacle downwards
             self.rect = self.image.get_rect(center=(self.x_pos, self.y_pos))
             if self.y_pos > 950:  # if obstacle is off the screen, kill
                 self.kill()
-        
-                
 
     # Game Variables and constants
     score_loop = 0
     VELOCITY = 5
-    lane_pos = []
-    lane_pos.append(115)
-    lane_pos.append(253)
-    lane_pos.append(436)
-    lane_pos.append(593)
+    lane_pos = [115, 253, 436, 593]
 
-    # intialize sprites
+    # Initialize sprites
     road = pygame.image.load('Road-1.png')
     road = pygame.transform.rotate(road, 90)
     road = pygame.transform.scale(road, (700, 925))
@@ -117,7 +106,7 @@ def game_loop():
 
     # Game Loop
     while True:
-    # Handle events
+        # Handle events
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
@@ -131,17 +120,21 @@ def game_loop():
             driver.move_left()
         else:
             driver.move_up()
-        
+
         # Check for collisions
         if pygame.sprite.spritecollide(driver, obstacle_group, False):
             game_over_screen()
 
         # Spawn obstacles
-        if random.random() < 0.02 and len(obstacle_group) < 4:  # Increase probability of obstacles spawning
+        if random.random() < 0.02 and len(obstacle_group) < 4:
+            # Increase probability of obstacles spawning
             obstacle_x = random.choice(lane_pos)
             obstacle_y = -100  # Spawn obstacles at the top of the screen
-            while any(abs(obstacle_x - obstacle.x_pos) < 73 for obstacle in obstacle_group):
-                obstacle_x = random.choice(lane_pos) # Generate new x_pos for obstacle until it's not on top of another one
+            while any(abs(obstacle_x - obstacle.x_pos) < 73 for
+                      obstacle in obstacle_group):
+                obstacle_x = random.choice(lane_pos)
+                # Generate new x_pos for obstacle until
+                # it's not on top of another one
             obstacle = Obstacle(obstacle_x, obstacle_y)
             obstacle_group.add(obstacle)
 
@@ -161,9 +154,10 @@ def game_loop():
         if score_loop == 6:
             score_loop = 0
             score += 1
-        
+
         # display player score on screen
-        player_score_text = font.render(f"Score: {(score/10)}", True, (255, 255, 255))
+        player_score_text = font.render(f"Score: {(score / 10)}", True,
+                                        (255, 255, 255))
         screen.blit(player_score_text, (55, 10))
 
         # Update display
@@ -177,15 +171,16 @@ def game_loop():
         # Control frame rate
         clock.tick(60)
 
-
         pygame.display.update()
+
 
 def game_over_screen():
     # Game Over Screen
     font = pygame.font.Font(None, 36)
     screen.fill((255, 255, 255))
     game_over_text = font.render("Game Over!", True, (0, 0, 0))
-    game_over_text2 = font.render("Would you like to restart or quit?", True, (0, 0, 0))
+    game_over_text2 = font.render("Would you like to restart or quit?", True,
+                                  (0, 0, 0))
     game_over_text3 = font.render("(Press R or Q)", True, (0, 0, 0))
     screen.blit(game_over_text, (150, 400))
     screen.blit(game_over_text2, (150, 550))
@@ -198,10 +193,10 @@ def game_over_screen():
         high_score = float(high_score_str)
     else:
         high_score = 0.0  # Set to a default value if file is empty
-    score_text = font.render(f"Your score was: {score/10}", True, (0, 0, 0))
-    if score/10 > high_score:
-        high_score = score/10
-        with open ("high_score.txt", "w") as f:
+    score_text = font.render(f"Your score was: {score / 10}", True, (0, 0, 0))
+    if score / 10 > high_score:
+        high_score = score / 10
+        with open("high_score.txt", "w") as f:
             f.write(str(high_score))
     high_score_text = font.render(f"High score: {high_score}", True, (0, 0, 0))
     screen.blit(score_text, (150, 450))
@@ -222,5 +217,6 @@ def game_over_screen():
                 elif event.key == pygame.K_q:
                     pygame.quit()
                     return
+
 
 game_loop()
